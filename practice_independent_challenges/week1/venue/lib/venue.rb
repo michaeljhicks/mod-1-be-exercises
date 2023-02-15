e1b89e918f5b4d96fff7320e48ef3fe2d27ab4cd
Michael Hicks <hicks.michael04@gmail.com>
2023-02-15 14:08:22 -0700

    complete week 1 venue

--- practice_independent_challenges/week1/venue/lib/venue.rb
+++ practice_independent_challenges/week1/venue/lib/venue.rb
@@ -1,2 +1,21 @@
 class Venue
+    attr_reader :name, :capacity, :patrons
+
+    def initialize(name, capacity)
+        @name = name 
+        @capacity = capacity 
+        @patrons = []
+    end
+
+    def add_patron(name) 
+        @patrons.push(name)
+    end 
+
+    def yell_at_patrons
+        yell = @patrons.map do |patron|
+            patron.upcase
+        end 
+        yell
+    end 
+
 end
